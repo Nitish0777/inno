6e1b5e48438be2a67aa05e9053299cbec1212f28
Nitish Nashine <nitishnashine@gmail.com>
2023-09-29 21:45:28 +0530

    login done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,13 @@
 import User from "../models/user.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import jwt from "jsonwebtoken";
+import randomstring from "randomstring";
 
+// register controller --------------- 1---------------- http://localhost:8000/api/users/register
 const registerUser = async (req, res) => {
   try {
     const { college, name, email, password } = req.body;
+    const hashedPassword = await hashPassword(password);
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({
@@ -10,7 +15,7 @@ const registerUser = async (req, res) => {
         message: "User already exists",
       });
     }
-    const user = new User({ college, name, email, password });
+    const user = new User({ college, name, email, password: hashedPassword });
     await user.save();
 
     // Send a success response to the client
@@ -28,4 +33,51 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+// login controller --------------- 2---------------- http://localhost:8000/api/users/login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide email and password",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect Password",
+      });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+        college: user.college,
+        _id: user._id,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error in logging in user",
+      error: error.message,
+    });
+  }
+};
+
+export { registerUser, loginUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { registerUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 console.log("Router loaded");
 
 router.post("/register", registerUser);
+
+router.post("/login", loginUser);
 
 export default router;
