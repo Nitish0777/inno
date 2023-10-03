e1f284126c594275a805554e698dd4798b801e00
Nitish Nashine <nitishnashine@gmail.com>
2023-10-03 21:54:56 +0530

    user schema modified

--- server/models/user.js
+++ server/models/user.js
@@ -14,21 +14,61 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  bio: {
+    type: String,
+    default: "",
+  },
   college: {
     type: String,
     required: true,
   },
   image: {
     type: String,
+    default: "",
   },
   mobileNo: {
     type: String,
+    default: "",
+  },
+  gender: {
+    type: String,
+    default: "",
+  },
+  sem_year: {
+    type: String,
+    default: "",
+  },
+  location: {
+    type: String,
+    default: "",
+  },
+  connection: {
+    type: Number,
+    default: 0,
+  },
+  link: {
+    website: {
+      type: String,
+      default: "",
+    },
+    linkdin: {
+      type: String,
+      default: "",
+    },
+    instagram: {
+      type: String,
+      default: "",
+    },
+    github: {
+      type: String,
+      default: "",
+    },
   },
   domain: {
     type: [String],
   },
-  gender: {
-    type: String,
+  programming: {
+    type: [String],
   },
   isMentor: {
     type: Number,
