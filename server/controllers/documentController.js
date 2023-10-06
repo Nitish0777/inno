0ec8dca0d70f8162cc2ffe82438447b05c84b7b8
Nitish Nashine <nitishnashine@gmail.com>
2023-10-06 10:51:45 +0530

    model of project done

--- server/controllers/documentController.js
+++ server/controllers/documentController.js
@@ -1,5 +1,12 @@
 const uploadDocumentation = async (req, res) => {
   try {
+    let filesUrl = null;
+
+    if (req.files) {
+      filesUrl = req.files.map((file) => {
+        return file.path;
+      });
+    }
   } catch (error) {
     return res.status(500).send({
       success: false,
