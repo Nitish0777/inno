715c37a2e0f9d4ea790eca6746cdbe29fbda6aff
Nitish Nashine <nitishnashine@gmail.com>
2023-10-09 10:14:25 +0530

    form done on Signup and Signin and sending to server

--- client/src/components/modal/SignUp.jsx
+++ client/src/components/modal/SignUp.jsx
@@ -5,11 +5,39 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+    try {
+      const res = axios.post(
+        `${process.env.REACT_APP_API}/api/users/register`,
+        {
+          college,
+          email,
+          name,
+          password,
+        }
+      );
+      if (res.data.success) {
+        console.log(res.data);
+        toast.success(res.data.message);
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       Sign Up form
-      <form>
+      <form onSubmit={handleSignUp}>
         <input
           type="text"
           placeholder="College"
@@ -29,6 +57,11 @@ const SignUp = () => {
           type="password"
           placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
         <input type="password" placeholder="Confirm Password" />
         <button type="submit">Sign Up</button>
