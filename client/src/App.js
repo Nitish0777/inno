b2e785630a31794bb2f1e3c9a0a02406419e18a2
Nitish Nashine <nitishnashine@gmail.com>
2023-10-09 21:43:11 +0530

    sign in working for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
 import "./App.css";
 import { ToastContainer } from "react-toastify";
-// import { Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./components/modal/SignIn";
 import SignUp from "./components/modal/SignUp";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <ToastContainer />
-      <h1>React App</h1>
-      <SignIn />
-      {/* <Routes>
-        <Route path="/" element={<SignIn />} />
-      </Routes> */}
-      <h1>Sign up</h1>
-      <SignUp />
-    </div>
+      <Routes>
+        {/* <Route path="/" element={<></>} /> */}
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Notifi.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { toast } from "react-toastify";
+
+const Notifi = () => {
+  toast.success("Verify your mail");
+  return <div>Notifi Page</div>;
+};
+
+export default Notifi;
