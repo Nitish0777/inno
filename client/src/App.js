2c96265235965d79ca81885d2c4b8a506cb248b2
Nitish Nashine <nitishnashine@gmail.com>
2023-10-11 21:42:45 +0530

    working on header footer component

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,21 @@ import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./components/modal/SignIn";
 import SignUp from "./components/modal/SignUp";
+import About from "./pages/homePage/about/About";
 
 function App() {
   return (
     <BrowserRouter>
       <ToastContainer />
       <Routes>
-        {/* <Route path="/" element={<></>} /> */}
+        <Route
+          path="/"
+          element={
+            <>
+              <About />
+            </>
+          }
+        />
         <Route path="/login" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
